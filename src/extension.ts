423acdf21be7679b948562786fe27615dc695cd7
DKim <32936089+kim222@users.noreply.github.com>
2023-12-11 11:38:15 +0000

    feat: Beep for every line change
    Now even if the indent is the same we will beep so long as the line
    has changed. This helps inform the user which indentation they're at

--- src/extension.ts
+++ src/extension.ts
@@ -7,14 +7,16 @@ const path = require('path')
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 let oldIndex = -1
+let oldLine = -1
 let config: PlayerConfig = {
 	macVol: 1,
 	winVol: 100,
 	linuxVol: 100
 };
-let basePath: string = path.join(__dirname, 'indentSounds');
 export function activate(context: vscode.ExtensionContext) {
+	let basePath: string = context.asAbsolutePath(path.join('indentSounds'));
 	oldIndex = -1
+	oldLine = -1
 	context.subscriptions.push(
 		vscode.window.onDidChangeTextEditorSelection(function (e) {
 			let editor = vscode.window.activeTextEditor
@@ -24,7 +26,10 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			let text = editor.document.lineAt(editor.selection.active.line).text
 			let index = -1
+			let currentLine = -1
+			currentLine = editor.document.lineAt(editor.selection.active.line).lineNumber
 			if (text.length != 0) {
+				console.debug(`Got some text: ${text}`)
 				index = editor.document.lineAt(editor.selection.active.line).firstNonWhitespaceCharacterIndex
 				let char = editor.document.lineAt(editor.selection.active.line).text[0]
 
@@ -37,17 +42,20 @@ export function activate(context: vscode.ExtensionContext) {
 			} else {
 				index = 0
 			}
+			console.debug(`Current line: ${currentLine} Old line: ${oldLine}`)
+			console.debug(`Current index: ${index} Old index: ${oldIndex}`)
 			//vscode.window.showInformationMessage(String(index))
-			if (index != oldIndex) {
-				reportIndex(index)
+			if (currentLine != oldLine) {
+				reportIndex(index, basePath)
 			}
+			oldLine = currentLine
 			oldIndex = index
 		}));
 
 }
-function reportIndex(index: Number) {
+function reportIndex(index: Number, basePath: string) {
 	//vscode.window.showInformationMessage("reported")
-	console.log("playing")
+	console.debug("playing " + basePath+"/"+index+".mp3")
 	player.play(basePath+"/"+index+".mp3", config)
 }
 export function deactivate() { }
